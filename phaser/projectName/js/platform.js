6eb4355f920e22cd0a8e1cce9b8bfcad51f898fb
wendellip <cip7@ucsc.edu>
2019-05-13 22:51:09 -0700

    First Prototype (missing state changes)

--- phaser/projectName/js/platform.js
+++ phaser/projectName/js/platform.js
@@ -8,18 +8,21 @@ game.physics.enable(this);
 this.enableBody = true;
 this.body.CollideWorldBounds = true;
 this.body.immovable = true;
+this.moving = false;
 }
 
 
 platform.prototype.update = function(hit,x, y)
 {
-	if(hit)
+	if(hit && !this.moving)
 	{
+		this.moving = true;
 		game.physics.arcade.moveToXY(this, x, y, 0, 2000);
 		game.time.events.add(2000, function ()
 		{
 			this.body.velocity.x = 0;
 			this.body.velocity.y = 0;
+			this.moving = false;
 		}
 		, this);
 	}
