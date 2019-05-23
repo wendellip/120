48d36efaf02d10514daabbc3269f4db32fd1c18c
TheObserverDoesGithub <50432100+TheObserverDoesGithub@users.noreply.github.com>
2019-05-23 15:24:31 -0700

    Add files via upload

---
+++ hand2.js
@@ -0,0 +1,33 @@
+hand2.prototype = Object.create(Phaser.Sprite.prototype);
+hand2.prototype.constructor = hand2;
+function hand2(game, key, frame, x, y)
+{
+Phaser.Sprite.call(this, game, x, y, key, frame);
+this.anchor.set(0.5,0.5);
+this.scale.setTo(.1);
+game.physics.enable(this);
+this.anchor.set(0,1);
+this.body.CollideWorldBounds = true;
+this.body.rotation=true;
+this.body.gravity.y = 900;
+this.isconnected==0;
+this.health=2;
+
+
+}
+
+
+hand2.prototype.update = function(colliding)
+{
+	//enable picking the arm
+	if(this.health == 2 && colliding)
+	{
+		return this.health;
+	}
+	else
+		return false;
+}
+
+function changepickup(){
+	this.setHealth(2);
+}
