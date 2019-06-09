e392bb21488f4f86b8ef97b640eb2ac6612d49b8
Brenden <BSatake@ucsc.edu>
2019-06-08 20:19:21 -0700

    reduced fps in enemy movement

--- phaser/EmpathyLabs/js/enemy.js
+++ phaser/EmpathyLabs/js/enemy.js
@@ -4,8 +4,8 @@ enemy.prototype.constructor = enemy;
 function enemy(game, key, frame, x, y, faceleft)
 {
 Phaser.Sprite.call(this, game, x, y, key, frame);
-this.animations.add('left', ['left01', 'left02'], 15, true);
-this.animations.add('right', ['righrt01', 'right02'], 15, true);
+this.animations.add('left', ['left01', 'left02'], 5, true);
+this.animations.add('right', ['righrt01', 'right02'], 5, true);
 this.animations.add('stand', ['middle01', 'middle02', 'middle03', 'middle01', 'middle04', 'middle05'], 15, true);
 this.animations.add('alarm', ['alarm01', 'alarm02', 'alarm03'], 15, false);
 this.animations.add('alarmloop', ['alarmloop01', 'alarmloop02', 'alarmloop03', 'alarmloop04', 'alarmloop05'], 15, true);
